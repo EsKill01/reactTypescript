de80c109f88cb89d692968bfdc95adca04e79bb0
Esteban Barboza <edbarboza20@gmail.com>
2022-08-05 20:09:42 -0600

    Custom hook

--- react-plus-typescript/src/components/AddToCart.tsx
+++ react-plus-typescript/src/components/AddToCart.tsx
@@ -33,3 +33,16 @@ export const WithAddToCartProps: React.FC<{children: (props: AddToCartProps) => 
             };
             return children({addToCart});
 }
+
+export const useAddToCart = () => {
+    const dispatch = useStateDispatch();
+            const addToCart: AddToCartProps['addToCart'] = (item: CartItem) => {
+                dispatch({
+                    type: "ADD_TO_CART",
+                    payload:{ item, }
+                })
+            };
+            return addToCart;
+
+            
+};
--- react-plus-typescript/src/components/Pizza.tsx
+++ react-plus-typescript/src/components/Pizza.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import PizzaCSS from './Pizza.module.css';
 import { Pizza } from '../types';
-import { AddToCartProps, withAddToCart } from './AddToCart';
+import { useAddToCart } from './AddToCart';
 
-interface Props extends AddToCartProps {
-    pizza: Pizza
+interface Props  {
+    pizza: Pizza;
 }
 
 
-const PizzaItem: React.FC<Props> = ({ pizza, addToCart }) => {
+const PizzaItem: React.FC<Props> = ({ pizza }) => {
+    const addToCart = useAddToCart();
     const handleAddToCartClick = () =>{
-        addToCart({id: pizza.id, name: pizza.name, price: pizza.price})
-    }
-    return <li className={PizzaCSS.container}>
+        addToCart({id: pizza.id, name: pizza.name, price: pizza.price});
+    };
+    return (<li className={PizzaCSS.container}>
         <h2>{pizza.name}</h2>
         <p>{pizza.description}</p>
         <p>{pizza.price}</p>
         <button type="button" onClick={handleAddToCartClick}>Add to Cart</button>
     </li>
-}
+    );
+};
 
-export default withAddToCart(PizzaItem);
+export default PizzaItem;
 
